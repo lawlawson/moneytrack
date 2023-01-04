4fa8b6e6e9bd0a53387d6fa863c576ba5dd4b12a
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-01-04 22:30:15 +0000

    Added Navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home/Home';
+import Navbar from './components/Navbar';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path='/'>
             <Home />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
-import styles from './Navbar.module.css'
+import { Link } from 'react-router-dom';
 
-import React from 'react'
+import styles from './Navbar.module.css';
 
-export const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>myMoney</li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
+        <li>
+          <Link to='/signup'>Sign up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
