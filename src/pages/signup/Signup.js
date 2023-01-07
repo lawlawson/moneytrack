3b13048bae45be5d15a88655c8bdd162440887be
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-01-07 17:22:05 +0000

    created signup page form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,44 @@
+import { useState } from 'react';
+
 import styles from './Signup.module.css';
 
-import React from 'react';
+export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
 
-const Signup = () => {
-  return <div>Signup</div>;
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default Signup;
+  return (
+    <form onSubmit={handleSubmit} className={styles['signup-form']}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='text'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <button className='btn'>Signup</button>
+    </form>
+  );
+}
