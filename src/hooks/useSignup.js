a2000c483a090ddd18cc205c1b6d50f3f472533e
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2023-02-06 10:51:55 +0000

    Completed creating signup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,21 +1,34 @@
-import { useState } from "react"
-import { projectAuthor } from "../firebase/config"
+import { useState } from 'react';
+import { projectAuth } from '../firebase/config';
 
 export const useSignup = () => {
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-  
+
   const signup = async (email, password, displayName) => {
     setError(null);
     setIsPending(true);
-    
+
     try {
+      //sign up user
+      const res = await projectAuth.createUserWithEmailAndPassword();
+      console.log(res.user);
 
+      if (!res) {
+        throw new Error('Could not complete signup');
+      }
+
+      //add display name to user
+      await res.user.updateProfile({ displayName });
+
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
     }
-    catch(err)
-  }
+  };
 
-  return { error, isPending, signup }
-}
-
-
+  return { error, isPending, signup };
+};
